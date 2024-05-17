38a5cce52939e1702f38d268f2f67e6181112eb4
kamalbuilds <geniusamansingh@gmail.com>
2024-05-17 23:59:44 +0530

    build err fix

--- frontend-modedevreward/app/withdrawnft/page.tsx
+++ frontend-modedevreward/app/withdrawnft/page.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers.zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
